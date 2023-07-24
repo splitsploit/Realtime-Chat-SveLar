fb0136c1aaf0d3af9addc32eb2dc40ce25e244d1
unknown <bayuxbca@gmail.com>
2023-07-24 21:40:48 +0700

    initiate message event

---
+++ app/Events/Message.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Message implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+
+    public $username;
+    public $message;
+    
+    public function __construct($username, $message)
+    {
+        return $this->username = $username;
+        return $this->message  = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn()
+    {
+        return ['chat'];
+    }
+
+    public function broadcastAs() 
+    {
+        return 'message';
+    }
+}
